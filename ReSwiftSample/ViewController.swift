daee9fc96f1eb009e6989cfd0317edc7d9bf1cbd
koogawa <koogawa.app@gmail.com>
2016-05-08 11:48:01 +0900

    Implement UI

--- ReSwiftSample/ViewController.swift
+++ ReSwiftSample/ViewController.swift
@@ -3,16 +3,22 @@
 //  ReSwiftSample
 //
 //  Created by koogawa on 2016/05/04.
-//  Copyright © 2016年 koogawa. All rights reserved.
+//  Copyright © 2016 koogawa. All rights reserved.
 //
 
 import UIKit
+import ReSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, StoreSubscriber {
+
+    @IBOutlet weak var label: UILabel!
+
+    private let store: Store<AppState> = Store<AppState>(reducer: CounterReducer(), state: AppState(counter: 0))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        store.subscribe(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func newState(state: AppState) {
+        label.text = String(state.counter)
+    }
 
+    @IBAction func didTapIncreaseButton(sender: AnyObject) {
+        store.dispatch(CountActionIncrease())
+    }
+
+    @IBAction func didTapDecreaseButton(sender: AnyObject) {
+        store.dispatch(CountActionDecrease())
+    }
 }
 
